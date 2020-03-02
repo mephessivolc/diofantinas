2ec5afc7a86639d9f165ae0471cf1930d39c2289
clovis caface <clovis.caface@gmail.com>
2020-03-02 11:50:15 -0300

    Refresh all in prime

--- prime.py
+++ prime.py
@@ -7,20 +7,33 @@ def verify_prime(num):
         Verifica se o numero recebido em num é primo.
     """
 
-    for count in range(2,num):
-        if num % count == 0:
-            return False
+    if num > 2:
+        for count in range(2,num):
+            if num % count == 0:
+                return False
 
-        return True
+    return True
 
 def create_prime_number_list(num):
     """
         Retorna a lista de numeros primos menores que o numero recebido em num
     """
-    _lst_prime = [2] # lista de primos
-    n = 2
 
-    while n < num+1:
+    _lst_prime = [] # lista de primos
+    n = 2
+    _num = num
+
+    try:
+        if isinstance(num, list):
+            _num = max(num)
+        elif isinstance(num, int) or isinstance(num, float):
+            _num = int(num)
+        elif isinstance(num, dict):
+            _num = int(max(num.values()))
+    except:
+        raise
+
+    while n < _num+1:
         if verify_prime(n):
             _lst_prime.append(n)
 
@@ -28,27 +41,19 @@ def create_prime_number_list(num):
 
     return _lst_prime
 
-def verify_prime_between_numbers(num):
-    """
-        Verifica se os numeros recebidos em num_a e num_b sao primos entre si
-    """
-
-    if isinstance(num, list):
-        _lst_prime = create_prime_number_list(max(num))
-        for arg in num:
-            print(num)
-            if arg in _lst_prime:
-                print('{} Não é numero primo'.format(arg))
-                return False
-    elif isinstance(num, int):
-        if not verify_prime(num):
-            print('Não é numero primo')
-            return False
-
-    print('É numero primo')
-    return True
-
 if __name__ == "__main__":
-    n = int(input("insira um numero "))
+    n = []
+    i = 1
+    while True:
+        try:
+            tmp = int(input("insira o {}º numero ".format(i)))
+        except:
+            break
+        n.append(tmp)
+        i = i+1
 
-    print(create_prime_number_list(n))
+    print('{} são os números inseridos'.format(n))
+    for i in n:
+        print("{} é numero primo? {}".format(i, verify_prime(i)))
+
+    print("lista de tabela de primos menores que {}: {}".format(max(n), create_prime_number_list(max(n))))
