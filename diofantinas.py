8d88f05fb176a6172128318b61b6e4ba8090fb16
clovis caface <clovis.caface@gmail.com>
2020-03-08 11:49:30 -0300

    Insert Result Matrix function

--- diofantinas.py
+++ diofantinas.py
@@ -9,13 +9,25 @@ class Diofante:
     def __init__(self, argv):
         args = Restricted(argv[1:])
         self.numbers = args.isrestricted()
+        self.order = len(self.numbers)
 
     def take_vec(self):
+        """
+            Cria a matriz b de valores inteiros
+        """
         vec = np.array(auxiliar.vec(self.numbers))
         return vec
 
+    def take_matrix(self):
+        """
+            Cria a matriz A de valores
+        """
+        matrix = auxiliar.matrix(self.take_vec(), self.order)
+
+        return matrix
+
     def show(self):
-        print(self.take_vec())
+        print(self.take_matrix())
 
 if __name__ == '__main__':
     t = Diofante(sys.argv)
