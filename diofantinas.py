6f6acc3f32e2f72cb37f3ecfa20440ea2a66e496
clovis caface <clovis.caface@gmail.com>
2020-05-01 19:42:08 -0300

    Inserindo funcao de escrita em linguagem Latex

--- diofantinas.py
+++ diofantinas.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import aux
+import pylatex
 
 from restricted import Restricted
 
@@ -9,6 +10,15 @@ initial_numbers=[0, 37, 89, 131, 401]
 class Diofante:
 
     def __init__(self, argv):
+        self.in_latex = False
+        if "-l" in argv:
+            self.in_latex = True
+            argv.remove('-l')
+            self.in_pdf = False
+            if "pdf" in argv:
+                self.in_pdf = True
+                argv.remove('pdf')
+
         if len(argv) == 1:
             argv = initial_numbers
 
@@ -32,8 +42,46 @@ class Diofante:
 
         return matrix
 
+    def print_latex(self):
+        pdf = pylatex.Document("default")
+
+        with pdf.create(pylatex.Section("Equações Diofantinas")) as section:
+            section.append("Equação:")
+            ultimo = self.numbers[-1]
+            eq = []
+            cont = 1
+            for i in self.numbers:
+                simbolo = "+"
+                if i == ultimo:
+                    simbolo = "= 1"
+                eq.append(pylatex.NoEscape(" {}x_{} {}".format(i,cont, simbolo)))
+                cont = cont + 1
+
+            section.append(pylatex.Math(data=eq))
+
+            text = "n = {}".format(self.order)
+            section.append(text)
+
+            m = pylatex.Matrix(self.take_vec(), mtype='b')
+            matrix = pylatex.Math(data=['b = ', m])
+            section.append(matrix)
+
+            m = pylatex.Matrix(self.take_matrix(), mtype='b')
+            matrix = pylatex.Math(data=['A = ', m])
+            section.append(matrix)
+
+
+        if self.in_pdf:
+            pdf.generate_pdf()
+
+        pdf.generate_tex()
+
     def show(self):
-        print("n = {}\n\nb =\n{}\n\na =\n{}".format(self.order, self.take_vec(), self.take_matrix()))
+        if self.in_latex:
+            self.print_latex()
+
+        else:
+            print("n = {}\n\nb =\n{}\n\na =\n{}".format(self.order, self.take_vec(), self.take_matrix()))
 
 if __name__ == '__main__':
     t = Diofante(sys.argv)
